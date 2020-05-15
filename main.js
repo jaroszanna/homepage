e5a3a27a9e52db112972447abe70feaedd42803f
Jarosz Anna <anna.wiktoria.jarosz@gmail.com>
2020-05-15 23:05:19 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const name = 'Anna';
+const age = 38;
+
+console.log(`Witaj na mojej stronie domowej! Nazywam się ${name}. Wszystkie treści są efektem udziału w szkoleniu "WTF: Co ten frontend" Macieja Korsana.`);
